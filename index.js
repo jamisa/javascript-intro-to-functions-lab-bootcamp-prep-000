6f77a78daec19e50ba3a260f630c21d07af6a670
Jamisa Fletcher <jamisafletcher@gmail.com>
2018-02-11 19:03:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,8 +21,7 @@ function sayHiToGrandma(string){
     return "I can't hear you!"
   } else if(string.toUpperCase() === string) {
     return "YES INDEED!"
-    
-  }
+}
   
  
   return string.toUpperCase()
