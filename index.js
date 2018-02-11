625250ca6c6ec295dc41557c7fb34bee64d7322a
Jamisa Fletcher <jamisafletcher@gmail.com>
2018-02-11 19:07:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,11 +22,11 @@ function sayHiToGrandma(string){
   } else if(string.toUpperCase() === string) {
     return "YES INDEED!"
 } else if (string === "I love you, Grandma."){
-  return ""
+  return "I love you, too."
 }
   
  
-  return string.toUpperCase()
+ 
 }
 
 
