1d9329eee5a3420a31d3db056c296d40c672956c
Jamisa Fletcher <jamisafletcher@gmail.com>
2018-02-11 18:24:04 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,31 @@
+
+
+function shout (string) {
+  return string.toUpperCase()
+}
+
+function wispher(string) {
+  return string.toLowerCase() 
+}
+
+function logShout(string) {
+  console.log ('hello'.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log('HELLO'.toLowerCase())
+}
+
+function sayHiToGrandma(string){
+ 
+  return string.toUpperCase()
+}
+
+function sayHiToGrandma(string) {
+  
+  return string.toLowerCase()
+}
+
+
+
+
